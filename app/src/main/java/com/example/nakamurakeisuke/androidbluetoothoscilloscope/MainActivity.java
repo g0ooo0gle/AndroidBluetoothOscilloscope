04d85c7105be01c315242af9bbbdcf3e067b0729
g0ooo0gle <mail.keisuke.nakamura01234@gmail.com>
2017-01-30 01:35:51 +0900

    テキストラベルにデータ表示
    テキストにデータ垂れ流すようにした

--- app/src/main/java/com/example/nakamurakeisuke/androidbluetoothoscilloscope/MainActivity.java
+++ app/src/main/java/com/example/nakamurakeisuke/androidbluetoothoscilloscope/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.nakamurakeisuke.androidbluetoothoscilloscope;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,15 +12,18 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView , statusView ;
-    LineChart linechart;
+    TextView textView , statusView;
+    LineChart signalchart;
     BluetoothSPP bt;
     Menu menu;
 
@@ -26,8 +32,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View content_view = findViewById(R.id.content_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        textView = (TextView)content_view.findViewById(R.id.textView);
+        statusView = (TextView)content_view.findViewById(R.id.statusView);
+        signalchart = (LineChart) content_view.findViewById(R.id.linechart);
+
+        bt = new BluetoothSPP(this);
+
+
+
+        if(!bt.isBluetoothAvailable()) {
+            Toast.makeText(getApplicationContext()
+                    , "Bluetooth is not available"
+                    , Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
+            public void onDataReceived(byte[] data, String message) {
+                textView.append(message + "\n");
+
+            }
+        });
+
+        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() {
+            public void onDeviceDisconnected() {
+                statusView.setText("Status : Not connect");
+                menu.clear();
+                getMenuInflater().inflate(R.menu.menu_connection, menu);
+            }
+
+            public void onDeviceConnectionFailed() {
+                statusView.setText("Status : Connection failed");
+            }
+
+            public void onDeviceConnected(String name, String address) {
+                statusView.setText("Status : Connected to " + name);
+                menu.clear();
+                getMenuInflater().inflate(R.menu.menu_disconnection, menu);
+
+            }
+        });
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -35,14 +85,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                if(!bt.isBluetoothAvailable()) {
+                    Toast.makeText(getApplicationContext()
+                            , "とーすとおしたけどBluetoothうごいてないぞ"
+                            , Toast.LENGTH_SHORT).show();
+                    finish();
+                }
             }
         });
+
+
+
+
     }
 
-    @Override
+    //@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        this.menu = menu;
+        getMenuInflater().inflate(R.menu.menu_connection, menu);
         return true;
     }
 
@@ -53,11 +115,81 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(id == R.id.menu_android_connect) {
+            bt.setDeviceTarget(BluetoothState.DEVICE_ANDROID);
+			/*
+			if(bt.getServiceState() == BluetoothState.STATE_CONNECTED)
+    			bt.disconnect();*/
+            Intent intent = new Intent(getApplicationContext(), DeviceList.class);
+            startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+        } else if(id == R.id.menu_device_connect) {
+            bt.setDeviceTarget(BluetoothState.DEVICE_OTHER);
+			/*
+			if(bt.getServiceState() == BluetoothState.STATE_CONNECTED)
+    			bt.disconnect();*/
+            Intent intent = new Intent(getApplicationContext(), DeviceList.class);
+            startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+        } else if(id == R.id.menu_disconnect) {
+            if(bt.getServiceState() == BluetoothState.STATE_CONNECTED)
+                bt.disconnect();
         }
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        bt.stopService();
+    }
+
+    public void onStart() {
+        super.onStart();
+        if (!bt.isBluetoothEnabled()) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
+        } else {
+            if(!bt.isServiceAvailable()) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_ANDROID);
+                //               setup();//送信用
+            }
+        }
+    }
+//送信用のやつ
+/*
+
+    public void setup() {
+        Button btnSend = (Button)findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(etMessage.getText().length() != 0) {
+                    bt.send(etMessage.getText().toString(), true);
+                    etMessage.setText("");
+                }
+            }
+        });
+    }
+*/
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if(resultCode == Activity.RESULT_OK)
+                bt.connect(data);
+        } else if(requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if(resultCode == Activity.RESULT_OK) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_ANDROID);
+                //setup();//とめてる
+            } else {
+                Toast.makeText(getApplicationContext()
+                        , "Bluetooth was not enabled."
+                        , Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 }
